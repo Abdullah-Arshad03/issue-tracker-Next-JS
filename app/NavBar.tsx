24d231c61efb00ab7e3b68f300c781a3986eccf5
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-04-17 19:05:05 +0500

    build the navbar!

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import { FaBug } from "react-icons/fa";
+import { usePathname } from 'next/navigation'; // this only works with the client component so we have to use the ' use client' above at the top
+import classNames from 'classnames';
 
 const NavBar = () => {
 
+    const currentPath = usePathname()
+    console.log(currentPath)
+    
     const links = [
         {
             label : 'Dashboard',
@@ -20,8 +27,12 @@ const NavBar = () => {
         <Link href=''> <FaBug/> </Link>
         <ul className='flex space-x-6 '>
             {links.map((link)=>(<>
-                <li key={link.href}> <Link href={link.href} className='text-zinc-500 hover:text-zinc-900 transition-colors'>{link.label}</Link> </li>
-            
+                <li key={link.href}> <Link href={link.href} className={classNames({
+                    'text-zinc-900' : link.href === currentPath,
+                    'text-zinc-500' : link.href !== currentPath,
+                    'hover:text-zinc-900 transition-colors' : true
+
+                })}>{link.label}</Link> </li>
             </>))}
         </ul>
     </nav>
---
+++ app/issues/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (<>
+  <h1>This is the Issues Page!</h1>
+    </>
+  )
+}
+
+export default IssuesPage
