986ba081454e612d074d0e16f376a08056e1f951
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-04-22 17:02:48 +0500

    Applied the Single Responsibility Principle to the Issues Detail Page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,49 +1,33 @@
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
-import React from 'react'
-import { Heading ,Text , Flex  , Card , Grid , Box , Button} from '@radix-ui/themes'
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import ReactMarkdown from 'react-markdown'
-import {Pencil2Icon} from '@radix-ui/react-icons'
-import Link from 'next/link'
-
+import prisma from "@/prisma/client";
+import { Box, Grid } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import EditIssueDetails from "./EditIssueDetails";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
-    params : {id : string}
+  params: { id: string };
 }
-const IssuesDetailPage = async({params : {id}} : Props) => {
+const IssuesDetailPage = async ({ params: { id } }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: Number(id) },
+  });
 
-   const issue = await prisma.issue.findUnique({
-        where : { id : Number(id)}
-    })
+  if (!issue) {
+    notFound();
+  }
 
-    if(!issue)
-        {
-            notFound()
-        }
-
-    console.log('this is the issue ', issue)
+  console.log("this is the issue ", issue);
   return (
-    <Grid columns={{initial : '1' , md: '2'}} gap='5'>
-      <Box className='max-w-xl'>
-      <Heading>{issue?.title}</Heading>
-      <Flex className='space-x-3' my='2'>
-     <IssueStatusBadge status={issue.status}/>
-      <Text>{issue?.createdAt.toDateString()}</Text>
-      </Flex>
-     <Card mt='4'>
-        <ReactMarkdown className='prose'>{issue?.description}</ReactMarkdown></Card> 
-        </Box>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box className="max-w-xl">
+        <IssueDetails issue={issue} />
+      </Box>
 
-        <Box>
-          
-          <Button>
-            <Pencil2Icon/>
-            <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-          </Button>
-        </Box>
+      <Box>
+        <EditIssueDetails issueId={issue.id} />
+      </Box>
     </Grid>
-  )
-}
+  );
+};
 
-export default IssuesDetailPage
+export default IssuesDetailPage;
