51a385365a5e50e8d7dc03155f71460805fa20e4
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-04-24 22:42:58 +0500

    Application Completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import LatestIssues from './LatestIssues'
 import IssueSummary from './IssueSummary'
 import prisma from '@/prisma/client'
 import IssueChart from './IssueChart'
+import { Grid ,Flex } from '@radix-ui/themes'
 
 const Home = async() => {
  const open =  await  prisma.issue.count({where: {status : 'OPEN'}})
@@ -12,7 +13,19 @@ const Home = async() => {
 
   return (<>
    {/* <LatestIssues/>  */}
-   <IssueChart open={open} inProgress={inProgress} closed={closed   }></IssueChart>
+   {/* <IssueChart open={open} inProgress={inProgress} closed={closed   }></IssueChart> */}
+
+  <Grid columns={{initial : '1'  , md : '2'}} gap='5'>
+    <Flex direction='column' gap='5'>
+      <IssueSummary open={open} inProgress={inProgress} closed={closed   }/>
+      <IssueChart open={open} inProgress={inProgress} closed={closed   }/>
+
+    </Flex>
+  <LatestIssues>
+
+  </LatestIssues>
+
+  </Grid>
  
     </>
   )
